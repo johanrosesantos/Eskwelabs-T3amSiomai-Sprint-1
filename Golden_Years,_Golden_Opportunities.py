8621492d4f51cedbe3417fa2a32b6a49586ebb10
Johan Santos <jrasantos22@gmail.com>
2024-05-25 15:19:56 +0800

    Update Golden_Years,_Golden_Opportunities.py

--- Golden_Years,_Golden_Opportunities.py
+++ Golden_Years,_Golden_Opportunities.py
@@ -1,24 +1,9 @@
 
 import streamlit as st
 
-base="light"
-
-st.markdown(
-    """
-    <style>
-    .reportview-container {
-        background: st.image("images/bg.jpg");
-    }
-   </style>
-    """,
-    unsafe_allow_html=True
-)
-
-
 st.set_page_config(
     page_title="Golden Years, Golden Opportunities",
     layout="wide"
 )
 
-st.write("# Golden Years, Golden Opportunities")
-st.image("assets/Title - Abby.png", use_column_width=True)
+st.image("images/1.png")
