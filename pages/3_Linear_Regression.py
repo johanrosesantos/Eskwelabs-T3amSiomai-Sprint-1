fedc1b77cf2e1e3acfbec1075c89a62b3b984478
Johan Santos <jrasantos22@gmail.com>
2024-05-25 15:54:36 +0800

    Update 3_Linear_Regression.py

--- pages/3_Linear_Regression.py
+++ pages/3_Linear_Regression.py
@@ -10,7 +10,7 @@ st.sidebar.header("What drives AAC user spending?")
 st.write("We estimated a linear regression model with total spending amount as the dependent variable. For the independent variables, we considered factors such as, the age group, location, the current season (i.e., dry or rainy), the time of day the transaction was made, and the RFM level. ")
 st.write("The total spending amount was log transformed, and all the independent variables were one-hot encoded.")
 
-st.image("images/lin_regression2.png", caption="Linear Regression Results", width = 800)
+st.image("images/lin_regression2.png", caption="Linear Regression Results", width = 600)
 
 st.markdown("## Interpretation")
 
