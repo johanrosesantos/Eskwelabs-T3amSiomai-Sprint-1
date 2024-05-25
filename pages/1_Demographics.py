93068e738531f622b5b54853e2ebb042dccbc936
Johan Santos <jrasantos22@gmail.com>
2024-05-25 17:51:37 +0800

    Update 1_Demographics.py

--- pages/1_Demographics.py
+++ pages/1_Demographics.py
@@ -4,11 +4,13 @@ st.set_page_config(
     page_title="How powerful is AAC's users spending?",
     layout="wide"
 )
+st.sidebar.image('images/adobologo.png', use_column_width=True)
+
 st.markdown("# How powerful is AAC's users spending?")
 st.sidebar.header("How powerful is AAC's users spending?")
 # st.image("title_slide.png", width = 800)
 
-st.header("AAC reported that about 6.49M USD was generated from transactions made between Jan. 1, 2020 to Dec. 7, 2021.")
+st.write("AAC reported that about 6.49M USD was generated from transactions made between Jan. 1, 2020 to Dec. 7, 2021.")
 st.write("This is around Php 377M with an exchange rate of 1 USD=Php58.")
 
 st.markdown("# Who are the AAC users?")
