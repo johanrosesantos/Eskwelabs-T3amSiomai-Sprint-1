43925c09268368b755d3c79b4fbf8871ff50f766
Johan Santos <jrasantos22@gmail.com>
2024-05-25 17:55:49 +0800

    Update 4_Insights.py

--- pages/4_Insights.py
+++ pages/4_Insights.py
@@ -4,6 +4,8 @@ st.set_page_config(
     page_title="Actionable Insights",
     layout="wide"
 )
+st.sidebar.image('images/adobologo.png', use_column_width=True)
+
 st.markdown("# What should AAC do?")
 st.sidebar.header("What should AAC do?")
 
