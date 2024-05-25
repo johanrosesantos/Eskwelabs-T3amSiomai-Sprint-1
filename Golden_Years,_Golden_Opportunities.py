7d451a88192aa831d3c2dc2bf87cbb6e2da2a202
Johan Santos <jrasantos22@gmail.com>
2024-05-25 17:17:13 +0800

    Update Golden_Years,_Golden_Opportunities.py

--- Golden_Years,_Golden_Opportunities.py
+++ Golden_Years,_Golden_Opportunities.py
@@ -1,5 +1,7 @@
-
 import streamlit as st
+
+# Sidebar content
+st.sidebar.image('images/adobo.logo.png', use_column_width=True)
 
 st.set_page_config(
     page_title="Golden Years, Golden Opportunities",
