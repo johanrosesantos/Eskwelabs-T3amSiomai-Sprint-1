1847823a5301ed26f39068fe80d091b8342c6fad
Johan Santos <jrasantos22@gmail.com>
2024-05-25 19:02:14 +0800

    Update 1_Demographics.py

--- pages/1_Demographics.py
+++ pages/1_Demographics.py
@@ -12,13 +12,16 @@ st.sidebar.header("How powerful is AAC's users spending?")
 st.write("AAC reported that about 6.49M USD was generated from transactions made between Jan. 1, 2020 to Dec. 7, 2021.")
 st.write("This is around Php 377M with an exchange rate of 1 USD=Php58.")
 
+st.write(" ")
+st.write(" ")
+
 st.markdown("# Who are the AAC users?")
 st.sidebar.header("Who are the AAC users?")
 
 st.header("Male users dominate the AAC clients by 94%")
 st.image("images/gender.png")
 
-st.header("Majority of AAC users hail from cities from Region III, IV-A & IV-B")
+st.header("Majority of AAC users hail from cities in Region III, IV-A & IV-B")
 st.image("images/cities.png")
 
 st.header("Top job categories of AAC users are from tourism, engineering and medical fields")
@@ -27,10 +30,13 @@ st.image("images/job.png", width=800)
 st.markdown("# What are the spending behavior of AAC users?")
 st.sidebar.header("What are the spending behavior of AAC users?")
 
-st.header("Users spend the moost on essentials including groceries, kids, pets and gas")
+st.write(" ")
+st.write(" ")
+
+st.header("Users spend the most on essentials including groceries, kids, pets and gas")
 st.image("images/catamt.png")
 st.write("Users also frequently spend onsite rather than online.")
 
-st.header("Meanwhile, users transact frequntly on groceries, shopping and gas")
+st.header("They also transact frequently on groceries, shopping and gas")
 st.image("images/catvol.png")
 st.write("Users also frequently spend onsite rather than online.")
