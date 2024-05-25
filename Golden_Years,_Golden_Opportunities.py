3f3d5f3e4516a468f853385b361b1d75fd71cb59
Johan Santos <jrasantos22@gmail.com>
2024-05-25 17:50:06 +0800

    Update Golden_Years,_Golden_Opportunities.py

--- Golden_Years,_Golden_Opportunities.py
+++ Golden_Years,_Golden_Opportunities.py
@@ -15,5 +15,5 @@ st.sidebar.write("Abby | Eugene | Gab | Johan | Nicole")
 st.image('images/1.png')
 
 # Main app content
-st.title("Adobo Advantage Cards' (AAC) Challenge")
+st.title("Adobo Advantage Card's (AAC) Challenge")
 st.write("AAC is a credit card provider seeking the help of a consultant, aiming to enhance customer satisfaction and drive business growth by understanding its customer behavior thru customer segmentation and its correlation to spending.")
