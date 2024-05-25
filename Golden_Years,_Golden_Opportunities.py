402ee2e2a31b391a4f3afda19c7c9ecb14ac6c65
wwckd <123295180+wwckd@users.noreply.github.com>
2024-05-25 14:21:21 +0800

    Update Golden_Years,_Golden_Opportunities.py

--- Golden_Years,_Golden_Opportunities.py
+++ Golden_Years,_Golden_Opportunities.py
@@ -20,11 +20,13 @@ st.set_page_config(
 
 st.write("# Golden Years, Golden Opportunities")
 # st.image("title_slide.png", width = 800)
-st.markdown(
-    """
-    Insert text here in markdown
-    # Heading
-    ## Subheading
-    - Bullets
-"""
-)
+# st.markdown(
+#     """
+#     Insert text here in markdown
+#     # Heading
+#     ## Subheading
+#     - Bullets
+# """
+# )
+
+st.image("assets/Title - Abby.png", use_column_width=True)
