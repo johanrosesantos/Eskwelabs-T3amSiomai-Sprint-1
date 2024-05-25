0c9db9f4eeb8b6ee43d19cc3a0953800ca80ecf4
wwckd <123295180+wwckd@users.noreply.github.com>
2024-05-25 12:51:36 +0800

    Update 4_Insights.py

--- pages/4_Insights.py
+++ pages/4_Insights.py
@@ -9,30 +9,3 @@ st.sidebar.header("What should AAC do?")
 # st.image("title_slide.png", width = 800)
 
 st.image("asssets/Actionable Strat - Abby.png", width = 800)
-
-# st.markdown(
-    """
-    # AAC can **drive business growth and boost customer satisfaction by:**
-    ## 1. Retain high-value RFM customers
-        - by offering personalized deals, loyalty rewards, and exclusive experiences
-
-    ## 1. Encourage increased expenditure for other customer segments
-        - Special offers and promotions should be positioned to be just above the customer’s current spending habits to encourage them to spend more
-
-    ## 1. Understand age preferences
-        - Gen X spends less, Silent generation spends more. Marketing campaigns should be tailored for each group’s preferences
-        - Partnerships with Health companies would be helpful for this age demographic
-
-    ## 1. Optimize location, holiday, and time-of-day strategies
-        - Since Visayas spends more, resources may be invested more into that region for campaigns and new stores
-        - Consider adjusting staffing or promotions during peak holidays and peak time-of-day
-    
-    ## 1. Expand customer base
-        - Targetting more female users would expand the business since females only make up 6% of the current customer base 
-
-    ## 1. Continuous monitoring
-        -Stay agile should the market dynamics and consumer preferences change
-
-
-"""
-# )
