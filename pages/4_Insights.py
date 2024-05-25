9b8b899b643ccb7e689d0442bd56bdb98f522f75
Johan Santos <jrasantos22@gmail.com>
2024-05-25 15:38:45 +0800

    Update 4_Insights.py

--- pages/4_Insights.py
+++ pages/4_Insights.py
@@ -1,18 +1,5 @@
 import streamlit as st
 
-base="light"
-
-st.markdown(
-    """
-    <style>
-    .reportview-container {
-        background: st.image("images/bg.jpg");
-    }
-   </style>
-    """,
-    unsafe_allow_html=True
-)
-
 st.set_page_config(
     page_title="Actionable Insights",
     layout="wide"
