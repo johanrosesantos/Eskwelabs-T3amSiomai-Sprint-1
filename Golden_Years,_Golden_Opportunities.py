21dba11fd9a5d0910f975a59253a2bda4983e6ac
Johan Santos <jrasantos22@gmail.com>
2024-05-25 17:29:13 +0800

    Update Golden_Years,_Golden_Opportunities.py

--- Golden_Years,_Golden_Opportunities.py
+++ Golden_Years,_Golden_Opportunities.py
@@ -4,7 +4,6 @@ st.set_page_config(
     page_title="Golden Years, Golden Opportunities",
     layout="wide",
     # Sidebar content
-    st.sidebar.image('images/adobo.logo.png', use_column_width=True)
-)
+    st.sidebar.image('images/adobo.logo.png', use_column_width=True))
 
 st.image("images/1.png")
